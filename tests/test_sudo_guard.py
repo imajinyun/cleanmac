56010dd2cd116b410a1e522228a70ce8892d74cc
imajinyun <imajinyun@gmail.com>
2026-06-26 08:16:58 +0800

    test(pytest): cover test mode helper blocks

--- tests/test_sudo_guard.py
+++ tests/test_sudo_guard.py
@@ -1,10 +1,20 @@
 from __future__ import annotations
 
+import pytest
+
 from cleancli import delete_ops
 from tests.helpers import cleanmac_test_env
 
 
-def test_test_mode_blocks_any_sudo_execution() -> None:
+@pytest.mark.parametrize(
+    "argv",
+    [
+        ["sudo", "true"],
+        ["sudo", "-n", "true"],
+        ["osascript", "-e", "return 1"],
+        ["launchctl", "print", "system"],
+    ],
+)
+def test_test_mode_blocks_privileged_and_automation_helpers(argv: list[str]) -> None:
     with cleanmac_test_env():
-        assert delete_ops.run_text_command(["sudo", "true"]) is None
-        assert delete_ops.run_text_command(["sudo", "-n", "true"]) is None
+        assert delete_ops.run_text_command(argv) is None
